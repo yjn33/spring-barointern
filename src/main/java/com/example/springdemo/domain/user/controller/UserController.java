feca00e0b13c998567cc8d80b298eb9da08c1c6e
yjn33 <yjn0032@gmail.com>
2025-07-07 13:00:39 +0900

    feat: 자신의 UserInfo 조회 기능 구현

--- src/main/java/com/example/springdemo/domain/user/controller/UserController.java
+++ src/main/java/com/example/springdemo/domain/user/controller/UserController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,6 +35,13 @@ public class UserController {
 		return ResponseEntity.ok(userService.updateUserInfo(authUser, requestDto));
 	}
 
+
+	@GetMapping("/users")
+	public ResponseEntity<UserInfoResponseDto> checkMyUserInfo(
+		@Auth AuthUser authUser
+	) {
+		return ResponseEntity.ok(userService.checkMyUserInfo(authUser));
+	}
 
 
 
