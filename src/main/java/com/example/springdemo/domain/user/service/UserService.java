1f5e9d895e4421ec3774cd16c1e62fe860fd519f
yjn33 <yjn0032@gmail.com>
2025-07-09 13:37:04 +0900

    Refactor: 존재하지않는 사용자 조회시 예외처리 리팩토링

--- src/main/java/com/example/springdemo/domain/user/service/UserService.java
+++ src/main/java/com/example/springdemo/domain/user/service/UserService.java
@@ -7,6 +7,7 @@ import com.example.springdemo.domain.user.entity.User;
 import com.example.springdemo.domain.user.repository.UserRepository;
 import com.example.springdemo.global.auth.annotation.Auth;
 import com.example.springdemo.global.auth.dto.AuthUser;
+import com.example.springdemo.global.common.exception.UserNotExistException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -39,7 +40,7 @@ public class UserService {
 	public UserInfoResponseDto checkMyUserInfo(AuthUser authUser) {
 
 		User user = userRepository.findById(authUser.getId())
-			.orElseThrow(() -> new IllegalArgumentException("해당 사용자는 존재하지 않습니다"));
+			.orElseThrow(UserNotExistException::new);
 
 		return new UserInfoResponseDto(user);
 	}
